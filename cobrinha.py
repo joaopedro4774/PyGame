637f6ca05f4f14ab57ce94fc3d8e87db7df7ee02
João Pedro <jpbsc2001@gmail.com>
2022-11-29 17:43:37 -0300

    Versão Final Correta

--- cobrinha.py
+++ cobrinha.py
@@ -14,8 +14,6 @@ UP = 0
 RIGHT = 1
 DOWN = 2
 LEFT = 3
-DOWN=2
-LEFT=3
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
@@ -23,7 +21,6 @@ pygame.display.set_caption('Maciel A Cobrinha')
 
 cobra = [(200, 200), (210, 200), (220,200)]
 design_cobra = pygame.Surface((10,10))
-design_cobra.fill((0,0,255)) 
 design_cobra.fill((0,0,255)) 
 
 possicao_fruta = on_grid_random()
@@ -84,51 +81,23 @@ while not game_over:
     if movimento_cobra == LEFT:
         cobra[0] = (cobra[0][0] - 10, cobra[0][1])
     
-screen.fill((0,0,0))
-screen.blit(fruta, possicao_fruta)
+    screen.fill((0,0,0))
+    screen.blit(fruta, possicao_fruta)
     
-for x in range(0, 600, 10):
+    for x in range(0, 600, 10): 
         pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, 600))
-for y in range(0, 600, 10):
+    for y in range(0, 600, 10): 
         pygame.draw.line(screen, (40, 40, 40), (0, y), (600, y))
     
-score_font = font.render('Pontuação: %s' % (score), True, (255, 255, 255))
-score_rect = score_font.get_rect()
-score_rect.topleft = (600 - 120, 10)
-screen.blit(score_font, score_rect)
+    score_font = font.render('Pontuação: %s' % (score), True, (255, 255, 255))
+    score_rect = score_font.get_rect()
+    score_rect.topleft = (600 - 120, 10)
+    screen.blit(score_font, score_rect)
     
-for pos in cobra:
-    screen.blit(design_cobra,pos)
-
-pygame.display.update()
-
-if movimento_cobra == UP:
-        cobra[0] = (cobra[0][0], cobra[0][1] - 10)
-if movimento_cobra == DOWN:
-        cobra[0] = (cobra[0][0], cobra[0][1] + 10)
-if movimento_cobra == RIGHT:
-        cobra[0] = (cobra[0][0] + 10, cobra[0][1])
-if movimento_cobra == LEFT:
-        cobra[0] = (cobra[0][0] - 10, cobra[0][1])
-    
-screen.fill((0,0,0))
-screen.blit(fruta, possicao_fruta)
-
-for x in range(0, 600, 10):
-        pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, 600))
-for y in range(0, 600, 10): 
-        pygame.draw.line(screen, (40, 40, 40), (0, y), (600, y))
-    
-score_font = font.render('Pontuação: %s' % (score), True, (255, 255, 255))
-score_rect = score_font.get_rect()
-score_rect.topleft = (600 - 120, 10)
-screen.blit(score_font, score_rect)
-    
-for pos in cobra:
+    for pos in cobra:
         screen.blit(design_cobra,pos)
 
-pygame.display.update()
-
+    pygame.display.update()
 
 while True:
     game_over_font = pygame.font.Font('freesansbold.ttf', 75)
