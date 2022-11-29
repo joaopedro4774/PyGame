93ff3b34b049fac42c9f9196d3f36d8ed0143ff2
Felipe Assed <felipeac5@al.insper.edu.br>
2022-11-29 16:55:28 -0300

    cobra_e_fruta

--- cobrinha.py
+++ cobrinha.py
@@ -1,10 +1,33 @@
 import pygame, random
 from pygame.locals import *
 
+def on_grid_random():
+    x = random.randint(0,59)
+    y = random.randint(0,59)
+    return (x * 10, y * 10)
+
+def c(c1, c2):
+    return (c1[0] == c2[0]) and (c1[1] == c2[1])
+
+
+UP = 0
+RIGHT = 1
+DOWN=2
+LEFT=3
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('Maciel A Cobrinha')
+
+cobra = [(200, 200), (210, 200), (220,200)]
+design_cobra = pygame.Surface((10,10))
+design_cobra.fill((0,0,255)) #Azul
+
+possicao_fruta = on_grid_random()
+fruta = pygame.Surface((10,10))
+fruta.fill((255,0,0))
+
+movimento_cobra = LEFT
 
 clock = pygame.time.Clock()
 
